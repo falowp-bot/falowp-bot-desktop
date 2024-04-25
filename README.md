3575452ea196d0451ac36ced735beb3bebde6b5c
冰璃然 <bingliran@126.com>
2024-04-25 14:49:02 +0800

    init

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
-# Tauri + React + Typescript
+# falowp-bot-desktop
+小花落Bot-QQ机器人框架-桌面管理程序
 
-This template should help get you started developing with Tauri, React and Typescript in Vite.
 
-## Recommended IDE Setup
-
-- [VS Code](https://code.visualstudio.com/) + [Tauri](https://marketplace.visualstudio.com/items?itemName=tauri-apps.tauri-vscode) + [rust-analyzer](https://marketplace.visualstudio.com/items?itemName=rust-lang.rust-analyzer)
+- 正在积极开发中
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,23 @@
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+use tauri::{App, Manager};
+
+use crate::command::{get_login_cache, login};
+
+mod command;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![
+            login,
+            get_login_cache
+        ])
+        .setup(|app| Ok(setup(app)))
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+fn setup(app: &mut App) {
+    app.app_handle();
+    return;
 }
